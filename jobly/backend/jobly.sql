95d71cc0ff75d8195a71b991c2d704b1d146ec08
mlghr <michael.rosenbaum1@gmail.com>
2021-02-01 11:41:19 -0800

    form update

--- jobly/backend/jobly.sql
+++ jobly/backend/jobly.sql
@@ -1,5 +1,5 @@
 \echo 'Delete and recreate jobly db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
+\prompt 'Return for yes > ' foo
 
 DROP DATABASE jobly_new;
 CREATE DATABASE jobly_new;
@@ -7,12 +7,3 @@ CREATE DATABASE jobly_new;
 
 \i jobly-schema.sql
 \i jobly-seed.sql
-
-\echo 'Delete and recreate jobly_test db?'
-\prompt 'Return for yes or control-C to cancel > ' foo
-
-DROP DATABASE jobly_test;
-CREATE DATABASE jobly_test;
-\connect jobly_test
-
-\i jobly-schema.sql
